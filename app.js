dd820cb46d5ea1eb7a5af600c5d0a852b6f0c404
Pomin Wu <pomin5@gmail.com>
2015-10-17 23:43:55 +0800

    add message box

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 window.app = (function () {
   function chatWindow (settings) {
     function draw (selection) {
-      log(window.chatlog.recentHistory())
       var messages = selection.selectAll('div.message')
         .data(window.chatlog.recentHistory(), function (d) { return d.id })
       messages
@@ -13,14 +12,36 @@ window.app = (function () {
       messages.exit()
         .remove()
     }
-
     return draw
   }
 
   function chatBox (settings) {
     function draw (selection) {
-    }
+      var messageBox = selection.selectAll('textarea.messageBox')
+        .data([''])
+        .text(function (d) { return d })
+      messageBox.enter().append('textarea')
+        .classed('messageBox', true)
+        .text(function (d) { return d })
 
+      var buttons = selection.selectAll('button')
+        .data([
+          { label: 'Submit', classNames: 'submit', onClick: handleSubmit }
+        ])
+      buttons.enter().append('button')
+        .attr('class', function (d) { return d.classNames })
+        .text(function (d) { return d.label })
+        .on('click', function (d) { return d.onClick() })
+
+      function handleSubmit () {
+        window.dispatcher.action({
+          type: 'addMessage',
+          payload: {
+            content: messageBox[0][0].value
+          }
+        })
+      }
+    }
     return draw
   }
 
@@ -28,24 +49,14 @@ window.app = (function () {
     function draw (selection) {
       selection.append('h1')
         .text('Chatroom')
-      selection.append('div')
+      var chatWindowContainer = selection.append('div')
         .classed('chatWindow', true)
-        .call(chatWindow())
-      selection.append('div')
+      var chatBoxContainer = selection.append('div')
         .classed('chatBox', true)
-        .call(chatBox())
 
       ;(function update (selection) {
-        var messages = selection.selectAll('div.message')
-          .data(window.chatlog.recentHistory(), function (d) { return d.id })
-        messages
-          .classed('message', true)
-          .text(function (d) { return d.content })
-        messages.enter().append('div')
-          .classed('message', true)
-          .text(function (d) { return d.content })
-        messages.exit()
-          .remove()
+        chatWindowContainer.call(chatWindow())
+        chatBoxContainer.call(chatBox())
         chatlog.on('update.app', update.bind(null, selection))
       })(selection)
     }
